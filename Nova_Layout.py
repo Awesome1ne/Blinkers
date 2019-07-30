57a645cc1fbd0bd221638df2259d15840ec0f645
Anson Davis <orangehead321@gmail.com>
2019-07-29 20:54:10 -0700

    Columns and rows aligned. Outer ring aligned. Component designators assigned. VCC and GND positioned. Need to add remaining resistors and position transistors and their inputs.

--- Nova_Layout.py
+++ Nova_Layout.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python
-import pcbnew
-import os
+#!/usr/bin/python3
 
-print("It worked!")
+#to run this script in Pcbnew enter:
+#   import os (1 time only)
+#   with open(os.path.expanduser('<path_to_script>')) as <a_name_for_the_file>: exec(<a_name_for_the_file>.read())
+# a ... should appear, then press Enter
+
+#import pcbnew
+#set a board object
+#board = pcbnew.GetBoard()
+
+print("The script executed.")
